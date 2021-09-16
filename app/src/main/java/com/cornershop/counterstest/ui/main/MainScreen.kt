32c8ff3beb3204da46f357996c0621906a09ee9b
Jonathan Meriño Bolivar <jmerino1204@gmail.com>
2021-09-15 21:08:38 -0500

    States main screen

--- app/src/main/java/com/cornershop/counterstest/ui/main/MainScreen.kt
+++ app/src/main/java/com/cornershop/counterstest/ui/main/MainScreen.kt
@@ -3,8 +3,10 @@ package com.cornershop.counterstest.ui.main
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.core.view.isVisible
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.cornershop.counterstest.R
 import com.cornershop.counterstest.databinding.ActivityMainScreenBinding
@@ -46,18 +48,24 @@ class MainScreen : ScopeActivity(), SwipeRefreshLayout.OnRefreshListener {
         binding.rvCounterInclude.rvCounter.layoutManager =
             LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         binding.rvCounterInclude.rvCounter.adapter = adapter
+
+        binding.contentError.btnRety.setOnClickListener { onRefresh() }
     }
 
     private fun updateUi(model: UiModel<List<Counter>>) {
 
         binding.progress.visibility = if (model is UiModel.Loading) View.VISIBLE else View.GONE
+        binding.contentError.root.isVisible = model is UiModel.Error
+        binding.noContent.root.isVisible = model is UiModel.Content
         binding.rvCounterInclude.swiperefresh.isRefreshing = model is UiModel.Loading
 
         when (model) {
             is UiModel.Content -> {
                 rvObserver =
                     RvEmptyObserver(binding.rvCounterInclude.rvCounter, binding.noContent.root)
-                //(binding.rvCounterInclude.rvCounter.adapter as RecyclerView.Adapter).registerAdapterDataObserver(rvObserver)
+                (binding.rvCounterInclude.rvCounter.adapter as RecyclerView.Adapter).registerAdapterDataObserver(
+                    rvObserver
+                )
                 Log.i("Main", model.data.toString())
             }
             is UiModel.Error -> {
